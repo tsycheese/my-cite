221a11ebeebc714bede8381af1d4b409cae4072d
梗小姐最好了 <1787129710@qq.com>
2024-11-16 16:49:24 +0800

    创建 Icon 组件

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,24 @@
 <template>
   <div>
     <h1>App 组件</h1>
-    <Avatar :url="avatarUrl" :size="100"></Avatar>
+    <Icon type="success"></Icon>
   </div>
 </template>
 
 <script>
-import Avatar from '@/components/Avatar';
+import Icon from './components/Icon.vue';
 
 export default {
   name: 'App', // 如果组件没有在注册时命名，则使用该名称
-  components: { Avatar },
-  data() {
-    return {
-      avatarUrl: 'https://avatars.githubusercontent.com/u/150910201?v=4',
-    };
+  components: {
+    Icon,
   },
 };
 </script>
+
+<style lang="less" scoped>
+.iconfont {
+  font-size: 30px;
+  color: #008c8c;
+}
+</style>
---
+++ src/components/Icon.vue
@@ -0,0 +1,48 @@
+<template>
+  <i class="iconfont" :class="fontClass"></i>
+</template>
+
+<script>
+const classMap = {
+  home: 'icon-zhuye',
+  success: 'icon-zhengqueshixin',
+  error: 'icon-cuowu',
+  close: 'icon-guanbi',
+  warn: 'icon-jinggao',
+  info: 'icon-xinxi',
+  blog: 'icon-blog',
+  code: 'icon-code',
+  about: 'icon-set_about_hov',
+  weixin: 'icon-weixin',
+  mail: 'icon-email-fill',
+  github: 'icon-github',
+  qq: 'icon-QQ-circle-fill',
+  arrowUp: 'icon-jiantou_liebiaoshouqi',
+  arrowDown: 'icon-jiantou_liebiaozhankai',
+  empty: 'icon-empty',
+  chat: 'icon-liuyan',
+};
+
+export default {
+  props: {
+    type: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    fontClass() {
+      return classMap[this.type];
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+@import url(//at.alicdn.com/t/c/font_4748072_75vgp5pjabi.css);
+
+.iconfont {
+  font-size: inherit;
+  color: inherit;
+}
+</style>
