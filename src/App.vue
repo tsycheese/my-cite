8a62ecf435768953458fc43a1940af7c0fbdb4d3
梗小姐最好了 <1787129710@qq.com>
2024-11-16 15:41:42 +0800

    创建头像组件

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,20 @@
 <template>
   <div>
     <h1>App 组件</h1>
+    <Avatar :url="avatarUrl" :size="100"></Avatar>
   </div>
 </template>
+
+<script>
+import Avatar from '@/components/Avatar';
+
+export default {
+  name: 'App', // 如果组件没有在注册时命名，则使用该名称
+  components: { Avatar },
+  data() {
+    return {
+      avatarUrl: 'https://avatars.githubusercontent.com/u/150910201?v=4',
+    };
+  },
+};
+</script>
---
+++ src/components/Avatar.vue
@@ -0,0 +1,26 @@
+<template>
+  <img class="avatar-img" :src="url" :style="{ width: size + 'px' }" />
+</template>
+
+<script>
+export default {
+  props: {
+    url: {
+      type: String, // 类型
+      required: true, // 属性必传
+    },
+    size: {
+      type: Number,
+      default: 50,
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.avatar-img {
+  border-radius: 50%;
+  object-fit: cover;
+  display: block;
+}
+</style>
