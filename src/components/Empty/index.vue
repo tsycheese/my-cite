59993b57a552d1cf93ed9bca8580f31136161d53
梗小姐最好了 <1787129710@qq.com>
2024-11-17 14:31:08 +0800

    尝试：开发 Empty 组件

---
+++ src/components/Empty/index.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="empty-container">
+    <div class="empty-content">
+      <div class="empty-icon" :style="{ fontSize: iconSize }">
+        <Icon type="empty"></Icon>
+      </div>
+      <div class="empty-text" :style="{ fontSize: textSize }">{{ text }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Icon from '@/components/Icon';
+
+export default {
+  components: {
+    Icon,
+  },
+  props: {
+    text: {
+      type: String,
+      default: '无数据',
+    },
+    size: {
+      type: Number,
+      default: 50,
+    },
+  },
+  computed: {
+    iconSize() {
+      return this.size + 'px';
+    },
+    textSize() {
+      return (this.size / 50) * 14 + 'px';
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.empty-container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.empty-content {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+// .empty-icon {
+//   font-size: 50px;
+// }
+
+.empty-text {
+  margin-top: 10px;
+  // font-size: 16px;
+  font-weight: bold;
+}
+</style>
---
+++ src/components/Empty/test.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <Empty :size="50" />
+  </div>
+</template>
+
+<script>
+import Empty from './';
+import '@/styles/global.less';
+
+export default {
+  name: 'App', // 如果组件没有在注册时命名，则使用该名称
+  components: {
+    Empty,
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.container {
+  width: 500px;
+  height: 500px;
+  background-color: antiquewhite;
+  margin: 100px auto;
+  padding: 20px;
+}
+
+.a {
+  width: 200px;
+  height: 100px;
+  background-color: #fff;
+}
+</style>
