658ec18c07318dd55dd434012bd324527198ddf7
梗小姐最好了 <1787129710@qq.com>
2024-11-18 19:55:22 +0800

    尝试：开发 Menu 组件

---
+++ src/components/Menu/index.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="menu-container">
+    <div
+      v-for="item in menuItems"
+      :key="item.path"
+      class="menu-item"
+      :class="{ active: item.matchCondition('/about') }"
+    >
+      <div class="menu-icon">
+        <Icon :type="item.icon" />
+      </div>
+      <div class="menu-text">{{ item.name }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Icon from '@/components/Icon';
+
+export default {
+  components: {
+    Icon,
+  },
+  data() {
+    return {
+      menuItems: [
+        {
+          name: '首页',
+          icon: 'home',
+          path: '/',
+          matchCondition: (currentPath) => currentPath === '/',
+        },
+        {
+          name: '文章',
+          icon: 'blog',
+          path: '/blog',
+          matchCondition: (currentPath) => currentPath.startsWith('/blog'),
+        },
+        {
+          name: '关于我',
+          icon: 'about',
+          path: '/about',
+          matchCondition: (currentPath) => currentPath === '/about',
+        },
+        {
+          name: '项目&成果',
+          icon: 'code',
+          path: '/project',
+          matchCondition: (currentPath) => currentPath === '/project',
+        },
+        {
+          name: '留言板',
+          icon: 'chat',
+          path: '/message',
+          matchCondition: (currentPath) => currentPath === '/message',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="less" scoped>
+@import url('~@/styles/global.less');
+
+.menu-container {
+  display: flex;
+  flex-direction: column;
+  color: @lightWords;
+  width: 100%;
+}
+
+.menu-item {
+  line-height: 40px;
+  display: flex;
+  cursor: pointer;
+  border-radius: 8px;
+}
+
+.menu-icon {
+  margin: 0 8px 0 30px;
+}
+
+.menu-item:hover,
+.menu-item.active {
+  color: white;
+  background-color: #2d2d2d;
+}
+</style>
---
+++ src/components/Menu/test.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <div class="content">
+      <Menu />
+    </div>
+  </div>
+</template>
+
+<script>
+import Menu from './';
+import '@/styles/global.less';
+
+export default {
+  name: 'App', // 如果组件没有在注册时命名，则使用该名称
+  components: {
+    Menu,
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.container {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.content {
+  width: 400px;
+  height: 400px;
+  background-color: black;
+}
+</style>
