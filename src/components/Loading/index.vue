157ac4dc50e3477ed4230686251c992f43756413
梗小姐最好了 <1787129710@qq.com>
2024-11-21 18:43:32 +0800

    添加 Loading 组件

---
+++ src/components/Loading/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <img class="loading-container" :src="loadingUrl" alt="" />
+</template>
+
+<script>
+import loadingUrl from '@/assets/loading.svg';
+
+export default {
+  data() {
+    return {
+      loadingUrl,
+    };
+  },
+};
+</script>
+
+<style lang="less" scoped>
+@import url('~@/styles/mixin.less');
+
+.loading-container {
+  .self-center();
+}
+</style>
