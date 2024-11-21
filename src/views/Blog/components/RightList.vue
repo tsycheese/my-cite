e6f820b9881aa50e4dd55681b10663a31c075809
梗小姐最好了 <1787129710@qq.com>
2024-11-21 20:23:40 +0800

    初步开发 Blog 页中的 RightList 组件

---
+++ src/views/Blog/components/RightList.vue
@@ -0,0 +1,54 @@
+<template>
+  <ul class="right-list-container">
+    <li v-for="(item, i) in list" :key="i">
+      <p @click="handleClick(item)">
+        <span class="title">{{ item.title }}</span>
+        <span class="desc">{{ item.desc }}</span>
+      </p>
+      <RightList
+        v-if="item.children"
+        :list="item.children"
+        @select="handleClick"
+      />
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'RightList',
+  props: {
+    list: {
+      type: Array,
+      required: true,
+    },
+  },
+  methods: {
+    handleClick(item) {
+      this.$emit('select', item);
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+@import url(~@/styles/var.less);
+
+.right-list-container {
+  .right-list-container {
+    margin-left: 1em;
+  }
+
+  p {
+    cursor: pointer;
+    min-height: 40px;
+    line-height: 40px;
+  }
+
+  .desc {
+    margin-left: 1em;
+    color: @gray;
+    font-size: 0.8em;
+  }
+}
+</style>
