aa795f8b613f6cc8bdda2805f6d1d65df7d6b48d
梗小姐最好了 <1787129710@qq.com>
2024-12-10 14:21:25 +0800

    删除不需要的文件

--- src/dist/config.dev.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.SERVER_URL = void 0;
-var SERVER_URL = 'http://localhost:7001';
-exports.SERVER_URL = SERVER_URL;
